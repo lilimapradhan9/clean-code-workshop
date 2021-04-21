9e1aaa58d5a3649ded195ba6feca537ef265a3ba
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-04-21 09:58:59 +0530

    Lilima | Add test cases for text statement

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -5,11 +5,84 @@ import org.junit.Test;
 
 public class CustomerTest {
     @Test
-    public void test() {
+    public void statementForCustomerWithRegularRental() {
+        Customer customer = new Customer("Dave");
+        customer.addRental(new Rental(new Movie("ABC", 0), 2));
 
-        int expected = 3;
+        String statement = customer.statement();
 
-        Assert.assertEquals(expected, 1 + 2);
+        Assert.assertEquals("Rental Record for Dave\n" +
+                "\tABC\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points", statement);
     }
 
+    @Test
+    public void statementForCustomerWithRegularRentalMoreThan2Days() {
+        Customer customer = new Customer("Dave");
+        customer.addRental(new Rental(new Movie("ABC", 0), 4));
+
+        String statement = customer.statement();
+
+        Assert.assertEquals("Rental Record for Dave\n" +
+                "\tABC\t5.0\n" +
+                "Amount owed is 5.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void statementForCustomerWithNewReleaseRental() {
+        Customer customer = new Customer("Dave");
+        customer.addRental(new Rental(new Movie("ABC", 1), 5));
+
+        String statement = customer.statement();
+
+        Assert.assertEquals("Rental Record for Dave\n" +
+                "\tABC\t15.0\n" +
+                "Amount owed is 15.0\n" +
+                "You earned 2 frequent renter points", statement);
+    }
+
+    @Test
+    public void statementForCustomerWithChildrenRental() {
+        Customer customer = new Customer("Dave");
+        customer.addRental(new Rental(new Movie("ABC", 2), 2));
+
+        String statement = customer.statement();
+
+        Assert.assertEquals("Rental Record for Dave\n" +
+                "\tABC\t1.5\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void statementForCustomerWithChildrenRentalMoreThan3days() {
+        Customer customer = new Customer("Dave");
+        customer.addRental(new Rental(new Movie("ABC", 2), 5));
+
+        String statement = customer.statement();
+
+        Assert.assertEquals("Rental Record for Dave\n" +
+                "\tABC\t4.5\n" +
+                "Amount owed is 4.5\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void statementForCustomerWithMultipleRentals() {
+        Customer customer = new Customer("Dave");
+        customer.addRental(new Rental(new Movie("ABC", 2), 5));
+        customer.addRental(new Rental(new Movie("DEF", 1), 3));
+        customer.addRental(new Rental(new Movie("PQR", 0), 2));
+
+        String statement = customer.statement();
+
+        Assert.assertEquals("Rental Record for Dave\n" +
+                "\tABC\t4.5\n" +
+                "\tDEF\t9.0\n" +
+                "\tPQR\t2.0\n" +
+                "Amount owed is 15.5\n" +
+                "You earned 4 frequent renter points", statement);
+    }
 }
